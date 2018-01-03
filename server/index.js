fd547b14de8fa6c9bf8d47370dafc0389904f690
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-01-02 22:59:40 -0800

    finish implementing the backend functions for Google OAuth with MongoDB

--- server/index.js
+++ server/index.js
@@ -1,34 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys.js');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(
-new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
-		}, 
-
-		(accessToken, refreshToken, profile, done) => {
-			console.log('access token', accessToken);
-			console.log('refresh token', refreshToken);
-			console.log('profile', profile);
-		}
-	)
-);
-
-app.get(
-	'/auth/google', 
-	passport.authenticate('google', {
-		scope: ['profile', 'email']
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, // how long the cookie exists in a browser --> 30 days
+		keys: [keys.cookieKey]
 	})
 );
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+app.use(passport.initialize()); // initialize passport
+app.use(passport.session()); // save sessions 
+
+require('./routes/authRoutes')(app); // Google OAuth Implementations
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
---
+++ server/services/passport.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys.js');
+
+const User = mongoose.model('users');
+
+// done function --> if successful, attaches the user to the request, also managed internally by passport
+
+passport.serializeUser((user, done) => {
+	done(null, user.id); // user.id = _id: {...}
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+		.then(user => {
+			done(null, user);
+		});
+});
+
+// passport.use excutes first, then serializeUser & deserializeUser
+passport.use(
+new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		}, 
+
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ googleId: profile.id })
+				.then((existingUser) => {
+					if(existingUser) {
+						// user already exists
+						done(null, existingUser); //null = no error
+					} else {
+						// make a new user
+						new User({ googleId: profile.id }) // New Model Instance
+							.save()
+							.then(user => done(null, user));
+					}
+				});
+		}
+	)
+);
