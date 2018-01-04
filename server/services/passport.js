8b938c2c2873d9c3ccce393da4a49d94779af1ae
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-01-03 17:01:31 -0800

    reverted passport.js

--- server/services/passport.js
+++ server/services/passport.js
@@ -20,26 +20,27 @@ passport.deserializeUser((id, done) => {
 
 // passport.use excutes first, then serializeUser & deserializeUser
 passport.use(
-  new GoogleStrategy(
-  {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
-  },
-    (accessToken, refreshToken, profile, done) => {
+new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback',
+			proxy: true
+		}, 
 
-      User.findOne({ googleId: profile.id }).then( (existingUser) => {
-          if (existingUser) {
-            // we already have the record with the given profile ID
-            done(null, existingUser); // no error & an the user we have
-          } else {
-            // we don't have a user with this ID. Make a new record
-            new User ({ googleId: profile.id })
-              .save()
-              .then( user => done(null, user));
-          }
-        })
-    }
-  )
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ googleId: profile.id })
+				.then((existingUser) => {
+					if(existingUser) {
+						// user already exists
+						done(null, existingUser); //null = no error
+					} else {
+						// make a new user
+						new User({ googleId: profile.id }) // New Model Instance
+							.save()
+							.then(user => done(null, user));
+					}
+				});
+		}
+	)
 );
