8e5268d8e64c6eefb4d357113d860d43373d88d1
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-03 18:24:29 -0800

    Reverted back to previous commits

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 
 module.exports = (app) => {
 	app.get(
-		'/auth/google', 
+		'/auth/google/callback', 
 		passport.authenticate('google', {
 			scope: ['profile', 'email']
 		})
