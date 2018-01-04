a3d4a3b66aab0076d0c30fd2e4e80c7100d5a142
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-01-03 16:22:39 -0800

    finished auth flow

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+// keys.js - figure out what set of credentials to return
+if(process.env.NODE_ENV == 'production') {
+	// In production - return prod set of keys
+	module.exports = require('./prod');
+} else {
+	// In development - return dev set of keys
+	module.exports = require('./dev');
+}
