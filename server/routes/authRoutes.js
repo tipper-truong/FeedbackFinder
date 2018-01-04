90c1b76003a9205f821cf7d7b0100dff745673f7
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-03 18:18:50 -0800

    Removed /callback from first route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 
 module.exports = (app) => {
 	app.get(
-		'/auth/google/callback', 
+		'/auth/google/', 
 		passport.authenticate('google', {
 			scope: ['profile', 'email']
 		})
