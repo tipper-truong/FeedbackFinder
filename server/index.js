14052fb79e35c0b093dce4c55f252386e434a8b7
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-01-05 23:53:41 -0800

    Working project, able to deploy to heroku properly

--- server/index.js
+++ server/index.js
@@ -28,7 +28,6 @@ require('./routes/billingRoutes')(app); // Stripe Billing Implementations
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-
   app.use(express.static('server/client/build'));
 
   // Express will serve up the index.html file
