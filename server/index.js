99d4236e4213b35c3f99f6eb0e0c7e010754cf79
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-01-05 23:39:37 -0800

    changed path

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ require('./routes/billingRoutes')(app); // Stripe Billing Implementations
 if(process.env.NODE_ENV == 'production') {
 	// Express will serve up production assets
 	// like our main.js file or main.css file
-	app.use(express.static(path.join(__dirname,'/client/build')));
+	app.use('/FeedbackFinder/server', express.static(path.join(__dirname, '/client/build')));
 
 	// Express will serve up the index.html file 
 	// if it doesn't recognize the route
