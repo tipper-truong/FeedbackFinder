d9ae0ee5081e2f04dc7e763fb5d17f6c4c33597f
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-01-05 23:42:55 -0800

    changed path to original

--- server/index.js
+++ server/index.js
@@ -25,17 +25,18 @@ app.use(passport.session()); // save sessions
 require('./routes/authRoutes')(app); // Google OAuth Implementations
 require('./routes/billingRoutes')(app); // Stripe Billing Implementations
 
-if(process.env.NODE_ENV == 'production') {
-	// Express will serve up production assets
-	// like our main.js file or main.css file
-	app.use('/FeedbackFinder/server', express.static(path.join(__dirname, '/client/build')));
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
 
-	// Express will serve up the index.html file 
-	// if it doesn't recognize the route
-	const path = require('path');
-	app.get('*', (req,res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
+  app.use(express.static('/client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
